42bc87099c5dd46774cee05cb9f05902d31cfa26
dustinn1235 <binhng1908@gmail.com>
2023-08-27 10:42:11 +0700

    didnt work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import Head from "next/head";
 import App from "./components/App";
 
 export default function Home() {
-  <Head>
-    <link rel="preload" href="logo.svg" as="image" />
-    <link rel="preload" href="cover.jpg" as="image" />
-  </Head>;
-
   return (
     <main>
       <App />
