636dbe36a7b77335eeacbef0660825e163516840
dustinn1235 <binhng1908@gmail.com>
2023-08-27 10:26:59 +0700

    try preload image

---
+++ src/app/components/App.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import Loading from "./Loading";
+import Navbar from "./Navbar";
+import Mainpage from "./Mainpage";
+import Products from "./Products";
+import About from "./About";
+import Clients from "./Clients";
+import Store from "./Store";
+import Slogan from "./Slogan";
+import Footer from "./Footer";
+
+const App = () => {
+  const [scrollable, setScrollable] = useState(false);
+
+  return (
+    <div
+      className="font-Poppins w-full h-screen absolute bg-black px-20 text-[#f8faf9] no-scrollbar"
+      style={{ overflowY: scrollable ? "auto" : "hidden" }}
+    >
+      <Loading setScrollable={setScrollable} />
+      {scrollable && (
+        <div className="w-full h-full flex flex-col items-center">
+          <Navbar />
+          <Mainpage />
+          <Products />
+          <About />
+          <Clients />
+          <Store />
+          <Slogan />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,15 @@
-"use client";
-
-import { useState } from "react";
-import Loading from "./components/Loading";
-import Navbar from "./components/Navbar";
-import Mainpage from "./components/Mainpage";
-import Products from "./components/Products";
-import About from "./components/About";
-import Clients from "./components/Clients";
-import Store from "./components/Store";
-import Slogan from "./components/Slogan";
-import Footer from "./components/Footer";
+import Head from "next/head";
+import App from "./components/App";
 
 export default function Home() {
-  const [scrollable, setScrollable] = useState(false);
+  <Head>
+    <link rel="preload" href="logo.svg" as="image" />
+    <link rel="preload" href="cover.jpg" as="image" />
+  </Head>;
 
   return (
-    <main
-      className="font-Poppins w-full h-screen absolute bg-black px-20 text-[#f8faf9] no-scrollbar"
-      style={{ overflowY: scrollable ? "auto" : "hidden" }}
-    >
-      <Loading setScrollable={setScrollable} />
-      {scrollable && (
-        <div className="w-full h-full flex flex-col items-center">
-          <Navbar />
-          <Mainpage />
-          <Products />
-          <About />
-          <Clients />
-          <Store />
-          <Slogan />
-          <Footer />
-        </div>
-      )}
+    <main>
+      <App />
     </main>
   );
 }
